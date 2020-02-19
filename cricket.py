dc58ff7a2e1616309192abbc9fbd82fcf3b28f64
mourice-oduor <otienomaurice364@gmail.com>
2020-02-19 11:42:49 -0500

    added new changes

--- cricket.py
+++ cricket.py
@@ -4,25 +4,27 @@ import requests
 import json
 
 
-root=Tk()
+cricket=Tk()
+cricket.title("My Live Cricket Scores")
 
-root.geometry("500x250")
+cricket.geometry("600x200")
 
 
-match_data=requests.get('http://cricapi.com/api/cricketScore?unique_id=(your unique id )&apikey=(your api key))
+match_data=requests.get('http://cricapi.com/api/cricketScore?unique_id=1201947&apikey=q5eB6MBPQkSKlbrXmzqL40oYUoT2')
 json_data=match_data.json()
 
 def times():
 	current_score=json_data['stat']
 	score.configure(text="current score :  "+current_score)
 	score.after(200,times)
+	
 
-
-
-
-
-score=Label(root,font=("time",15,"bold"),bg="white")
+score=Label(cricket,font=("time",15,"bold"),fg="red", bg="blue")
 score.grid(row=2,column=2,pady=25,padx=100)
 times()
 
-root.mainloop()
+live=Label(cricket,text="LIVE CRICKET SCORES",font=("italics 24 bold"),fg="green")
+live.grid(row=0,column=2)
+
+
+cricket.mainloop()
